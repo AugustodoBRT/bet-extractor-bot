084f3ab269880c3b22a28837774c144142df7dd2
AugustodoBRT <augustodobrt8@gmail.com>
2026-08-04 10:52:24 -0300

    Atualizando aba para Agosto26

--- google_sheets.py
+++ google_sheets.py
@@ -21,7 +21,7 @@ try:
     spreadsheet = client.open("GeogeTips")
     
     # abre a aba correta
-    NOME_ABA = "Julho26"
+    NOME_ABA = "Agosto26"
     sheet = spreadsheet.worksheet(NOME_ABA)
     print(f"✅ Conectado à planilha GeogeTips, aba: {NOME_ABA}")
         
